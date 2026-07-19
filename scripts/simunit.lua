f31c5715e41e41fd58391d57962c86a10058f8bb
Phyrax <124089724+Phyrax1@users.noreply.github.com>
2026-07-19 11:25:36 +0200

    Fix TRG_LOS_REFRESH not firing correctly for hacks

--- scripts/simunit.lua
+++ scripts/simunit.lua
@@ -115,6 +115,18 @@ rawset(
             end
         end)
 
+-- fix mission hooks not firing when spotting cell tags via hacking a camera
+local oldSetPlayerOwner = simunit.setPlayerOwner
+simunit.setPlayerOwner = function(self, player, ...)
+	local oldOwner = self._parent
+	oldSetPlayerOwner(self, player, ...)
+	if self:isValid() and oldOwner and self._parent ~= oldOwner and self._parent == player then
+		local cells = {}
+		self:getSim():getLOS():getVizCells(self:getID(), cells)
+		self:getSim():triggerEvent(simdefs.TRG_LOS_REFRESH, { seer = self, cells = cells })
+	end
+end
+
 local oldAddTag = simunit.addTag
 function simunit:addTag(tag, ...)
     if self:getUnitData().id == "data_card" and tag == "access_card_obj" then
